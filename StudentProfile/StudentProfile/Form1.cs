40ba2e1b4de6f64d19b7eadf919847c768d12522
Kim-Jonas-Malapo <finmckill@gmail.com>
2026-09-13 21:24:23 +0800

    Implement year update b

--- StudentProfile/StudentProfile/Form1.cs
+++ StudentProfile/StudentProfile/Form1.cs
@@ -20,7 +20,7 @@ namespace StudentProfile
             {
                 lblName.Text = "Name: John Doe";
                 lblID.Text = "ID: 12345";
-                lblYear.Text = "Year: 5th Year";
+                lblYear.Text = "Year: 4th Year";
             }
 
             else if (studentID == "12344"){
